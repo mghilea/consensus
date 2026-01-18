b3f6ed3088875fabdcba06da23342cc7d0b74133
mghilea <mghilea@colgate.edu>
2026-01-18 09:58:10 -0700

    Set SSHD file descriptor limits

--- scripts/lib/sharding_codebase.py
+++ scripts/lib/sharding_codebase.py
@@ -62,8 +62,8 @@ class ShardingCodebase(ExperimentCodebase):
                 "mkdir -p /etc/systemd/system/sshd.service.d && "
                 "echo -e '[Service]\\nLimitNOFILE=%d' > /etc/systemd/system/sshd.service.d/limits.conf && "
                 "systemctl daemon-reexec && "
-                "systemctl restart sshd && " % config['sshd_file_descriptors']
-            )
+                "systemctl restart sshd && "
+            ) % config['sshd_file_descriptors']
 
         # Raise client process FD limit if configured
         if 'max_file_descriptors' in config:
@@ -203,8 +203,8 @@ class ShardingCodebase(ExperimentCodebase):
                 "mkdir -p /etc/systemd/system/sshd.service.d && "
                 "echo -e '[Service]\\nLimitNOFILE=%d' > /etc/systemd/system/sshd.service.d/limits.conf && "
                 "systemctl daemon-reexec && "
-                "systemctl restart sshd && " % config['sshd_file_descriptors']
-            )
+                "systemctl restart sshd && "
+            ) % config['sshd_file_descriptors']
 
         # Raise replica process FD limit if configured
         if 'max_file_descriptors' in config:
